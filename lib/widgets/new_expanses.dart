93b4bd7cb7470fe51fa11c6262f27caaf9c6ee29
Aimad Moustajil <moustajil.dev@gmail.com>
2025-01-14 14:38:25 +0100

    adding selecyed date time

--- lib/widgets/new_expanses.dart
+++ lib/widgets/new_expanses.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
 
 class NewExpanses extends StatefulWidget {
   const NewExpanses({super.key});
@@ -8,6 +11,31 @@ class NewExpanses extends StatefulWidget {
 }
 
 class _NewExpansesState extends State<NewExpanses> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _persentDatePiker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final lastDate = DateTime(now.year + 100, now.month, now.day);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: lastDate);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,10 +43,60 @@ class _NewExpansesState extends State<NewExpanses> {
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
             decoration: InputDecoration(
               label: Text("Title"),
             ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  maxLength: 50,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: "\$ ",
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(_selectedDate == null
+                        ? "No Date Selected"
+                        : formatter.format(_selectedDate!)),
+                    IconButton(
+                      onPressed: _persentDatePiker,
+                      icon: Icon(Icons.calendar_month),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text("save expanse"),
+              )
+            ],
           )
         ],
       ),
