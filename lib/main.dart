b2e23d3967b03ded16cb7f9f12b317c9045c1d88
Aimad Moustajil <moustajil.dev@gmail.com>
2025-01-15 16:34:45 +0100

    fix oriantation of screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ex_tr/widgets/expanses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.black);
 var kDarkColorShema = ColorScheme.fromSeed(
@@ -8,36 +9,41 @@ var kDarkColorShema = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  ).then((fn) {
+    runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+            colorScheme: kColorScheme,
+            cardTheme: const CardTheme().copyWith(
+              color: kDarkColorShema.secondaryContainer,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            )),
+        theme: ThemeData().copyWith(
           colorScheme: kColorScheme,
-          cardTheme: const CardTheme().copyWith(
-            color: kDarkColorShema.secondaryContainer,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-          )),
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer),
-        cardTheme: CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          appBarTheme: AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer),
+          cardTheme: CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    color: kColorScheme.onSecondaryContainer,
+                    fontSize: 16),
+              ),
         ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: kColorScheme.primaryContainer),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  color: kColorScheme.onSecondaryContainer,
-                  fontSize: 16),
-            ),
+        themeMode: ThemeMode.system,
+        home: Expanses(),
       ),
-      themeMode: ThemeMode.system,
-      home: Expanses(),
-    ),
-  );
+    );
+  });
 }
